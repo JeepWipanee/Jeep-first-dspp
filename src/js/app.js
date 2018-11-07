da2927d7a956f3dc42fa58d776cbbe920552a08a
Wipanee Khamluewong <wipanee_k@BKC.CO.TH>
2018-11-07 11:41:54 +0700

    เพิ่ม initWeb3 และ function displayAccountInfo()

--- src/js/app.js
+++ src/js/app.js
@@ -4,25 +4,29 @@ App = {
 
      init: function() {
           
-            var articleRow = $('#articlesRow');
-            var articleTemplate = $('#articleTemplate');
+            // var articleRow = $('#articlesRow');
+            // var articleTemplate = $('#articleTemplate');
             
-            articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
-            articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
-            articleTemplate.find('.article-price').text('10.24');
-            articleTemplate.find('.article-seller').text('ชื่อผู้ขาย');
+            // articleTemplate.find('.panel-title').text('สินค้าชิ้นที่ 1');
+            // articleTemplate.find('.article-description').text('รายละเอียดสินค้าชิ้นที่ 1');
+            // articleTemplate.find('.article-price').text('10.24');
+            // articleTemplate.find('.article-seller').text('วิภาณี');
 
-            articleRow.append(articleTemplate.html());
+            // articleRow.append(articleTemplate.html());
 
           return App.initWeb3();
      },
 
      initWeb3: function() {
-          /*
-           * Replace me...
-           */
+      App.web3Provider  = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+      App.displayAccountInfo();
 
           return App.initContract();
+     },
+
+     displayAccountInfo: function(){
+
      },
 
      initContract: function() {
